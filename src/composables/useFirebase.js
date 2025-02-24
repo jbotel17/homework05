d9056e0bb4f551270308bc6ddca947fc271582a5
Jeremyah Botello <jbotel17@stu.southtexascollege.edu>
2025-02-24 03:34:10 -0600

    redo all homework since the fisrt one crashed when i run the npm build and did not comitted some changes

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_kTvlqYtg28j_627T0S6uZjN5glZgfpE",
+  authDomain: "homework05-379da.firebaseapp.com",
+  projectId: "homework05-379da",
+  storageBucket: "homework05-379da.firebasestorage.app",
+  messagingSenderId: "324291734113",
+  appId: "1:324291734113:web:ccc544e46ee2435f121820"
+};
+
+// Initialize Firebase
+ export const firebaseApp = initializeApp(firebaseConfig);
